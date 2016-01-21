a5f976724345de9a852ab39615b4fed3fb30fa1e
Paul Boyle <pboyle17@gmail.com>
2016-01-21 14:04:41 -0600

    messing around with jade

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/',function(req,res){
+  var firstName=req.body.firstName
+  res.render('post');
+});
+
 module.exports = router;
